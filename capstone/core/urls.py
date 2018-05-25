b08a4fe2b54e9fed693c5a872ff620a99f959c89
kamgamboa <kmllgmb@gmail.com>
2018-05-25 23:53:24 +0800

    Added "/" at the end of paths for core.urls

--- capstone/core/urls.py
+++ capstone/core/urls.py
@@ -5,8 +5,8 @@ from core import views
 app_name = 'core'
 
 urlpatterns = [
-    path('login', views.SignInView.as_view(), name='login'),
-    path('logout', views.logout_view, name='logout'),
+    path('login/', views.SignInView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
     # BNS index
     path('bns/', views.bns_index, name='bns-index'),
