bdb82f7216166ebe8523b3fcc1d125713ce17437
Larenz Lojo <maclarenz_lojo@dlsu.edu.ph>
2018-06-09 22:29:11 +0800

    placeholder
    added placeholders in forms

--- capstone/core/templatetags/tags.py
+++ capstone/core/templatetags/tags.py
@@ -3,8 +3,4 @@ register = template.Library()
 
 @register.filter(name='addcssclass')
 def addcssclass(field, css):
-   return field.as_widget(attrs={"class":css})
-
-@register.filter(name='addcssph')
-def addcssph(field, css):
-   return field.as_widget(attrs={"placeholder":css})
+   return field.as_widget(attrs={"class":css, "placeholder":field.label})
