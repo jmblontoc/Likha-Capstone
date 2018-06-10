63b243b8aa05f479aa0d50b9b9a078059dd185be
jmblontoc <37819032+jmblontoc@users.noreply.github.com>
2018-06-10 15:38:22 +0800

    added unread notification count

--- capstone/core/context_processors.py
+++ capstone/core/context_processors.py
@@ -14,3 +14,18 @@ def get_notifications(request):
     return {
         'notifications': None
     }
+
+
+def get_unread_notifications(request):
+
+    if not request.user.is_anonymous:
+
+        unread_count = Notification.objects.filter(profile_to=Profile.objects.get(user=request.user), is_read=False)
+
+        return {
+            'unread_count': unread_count.count()
+        }
+
+    return {
+        'unread_count': None
+    }
