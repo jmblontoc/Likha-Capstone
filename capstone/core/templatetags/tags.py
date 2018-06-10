c7da897f708d6b26bb2146e957e5181b8e058da3
Larenz Lojo <maclarenz_lojo@dlsu.edu.ph>
2018-06-10 13:15:28 +0800

    finished add family profile
    done with bns except opt and fhsis

--- capstone/core/templatetags/tags.py
+++ capstone/core/templatetags/tags.py
@@ -2,5 +2,9 @@ from django import template
 register = template.Library()
 
 @register.filter(name='addcssclass')
-def addcssclass(field, css):
-   return field.as_widget(attrs={"class":css, "placeholder":field.label})
+def addcssclass(field):
+   return field.as_widget(attrs={"class":"form-control", "placeholder":field.label})
+
+@register.filter(name='field_type')
+def field_type(field):
+    return field.field.widget.__class__.__name__
