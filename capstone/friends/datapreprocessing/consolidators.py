bf82053d73ce5a14005bb703ef1da3dcca508c65
jmblontoc <37819032+jmblontoc@users.noreply.github.com>
2018-06-11 04:40:41 +0800

    worked on algorithms for datapreprocessing

--- capstone/friends/datapreprocessing/consolidators.py
+++ capstone/friends/datapreprocessing/consolidators.py
@@ -1,22 +1,39 @@
+from datetime import datetime
+
 from django.apps import apps
 
-def get_value(metric):
+from datainput.models import HealthCareWasteManagement, OPTValues, UnemploymentRate
 
-    # check if may hyphen
+
+def get_value(metric):
 
     phrase = metric.split("|")
 
     # get model
-    model = apps.get_model('datainput', get_model(metric))
-    print(model)
+    str_model = get_model(metric)
+    model = apps.get_model('datainput', str_model)
+
 
     # get field
-    x = model._meta.get_field(verbose_name=phrase[1])
-    print(x)
+    # not applicable for NutritionalStatuses
+
+    if str_model == 'OPTValues':
+        print('todo')
+
+    elif str_model == 'HealthCareWasteManagement':
+        field = get_field(model, phrase[1])
+        return get_total_count(model, field, True, False, False)
+
+    elif str_model == 'InformalSettlers':
+        field = get_field(model, phrase[1])
+        return get_total_count(model, field, False, True, False)
+
+    elif str_model == 'UnemploymentRate':
+        return UnemploymentRate.objects.filter(date__year=datetime.now().year)[0].rate
 
     # args = is yearly, is monthly, is barangay level,
 
-    return len(phrase)
+    return 'wait lang'
 
 
 def get_model(metric):
@@ -35,13 +52,13 @@ def get_model(metric):
             model = 'FamilyProfileLine'
         elif 'Health Care Waste Management' in first:
             model = 'HealthCareWasteManagement'
+        elif 'Informal Settlers' in first:
+            model = 'InformalSettlers'
 
     elif len(phrase) == 1:
 
         if 'Unemployment Rate' in first:
             model = 'UnemploymentRate'
-        elif 'Informal Settlers' in first:
-            model = 'InformalSettlers'
 
     elif len(phrase) == 3:
 
@@ -53,5 +70,44 @@ def get_model(metric):
     return model
 
 
-def get_total(model, field):
-    pass
+def get_field(model, verbose):
+
+    if not model is OPTValues:
+        fields = []
+        for field in model._meta.get_fields():
+            if field.verbose_name == verbose.strip():
+                fields.append(field)
+
+        str_field =  str(fields[0]).split(".")
+        return str_field[2]
+
+    return None
+
+
+def get_total_count(model, field, is_annual, is_monthly, is_barangay):
+
+    if is_annual:
+        if not is_barangay:
+
+            # health care waste management
+            count = 0
+            records = model.objects.all().filter(date__year=datetime.now().year)
+
+            for record in records:
+                count = count + getattr(record, field)
+
+            return count
+
+    else:
+        if not is_barangay:
+
+            # informal settlers
+            count = 0
+            records = model.objects.all().filter(date__year=datetime.now().year)
+
+            for record in records:
+                count = count + getattr(record, field)
+
+            return count
+
+
